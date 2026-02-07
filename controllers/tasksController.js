ca34d5835d82389f3be5811a679bca945ff690db
paresh2421 <pareshnair2411@gmail.com>
2026-02-07 16:01:14 +0530

    adds task creation and display api functionalities

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -2,10 +2,15 @@ import { StatusCodes } from "http-status-codes";
 import TaskSchema from "../models/Task.js";
 
 const createTask = async (req, res) => {
-  res.send("createTask");
+  req.body.userId = req.user.userID;
+  const task = await TaskSchema.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ task });
 };
 const getAllTasks = async (req, res) => {
-  res.send("getAllTasks");
+  const user = req.user.userID;
+  const tasks = await TaskSchema.find({ userId: user }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ tasks, count: tasks.length });
 };
 const updateTask = async (req, res) => {
   res.send("updateTask");
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -13,6 +13,7 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     req.user = { userID: payload.userId, name: payload.name };
+    next()
   } catch (error) {
     throw new UnauthenticatedError("Authentication Invalid");
   }
